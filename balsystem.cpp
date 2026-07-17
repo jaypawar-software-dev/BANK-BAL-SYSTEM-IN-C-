65e6ea2e92d29ad797a29e8a448b6d9cf1e593a8
JAY PAWAR <245702788+jaypawar-software-dev@users.noreply.github.com>
2026-07-17 21:00:18 +0530

    Add files via upload

--- balsystem.cpp
+++ balsystem.cpp
@@ -5,13 +5,13 @@ using namespace std;
 class BankAccount {
 private:
     // Private data member: cannot be accessed directly from outside the class
-    double balance; 
+    float balance; 
 
 public:
     string accountHolder;
 
     // Constructor: initializes the object with holder's name and initial balance
-    BankAccount(string name, double initialBalance) {
+    BankAccount(string name, float initialBalance) {
         accountHolder = name;
         if (initialBalance >= 0) {
             balance = initialBalance;
@@ -21,12 +21,12 @@ public:
     }
 
     // Getter Method: provides safe, read-only access to the private balance variable
-    double getBalance() {
+    float getBalance() {
         return balance;
     }
 
     // Setter/Member Method: safely modifies the balance after validating the input
-    void deposit(double amount) {
+    void deposit(float amount) {
         if (amount > 0) {
             balance = balance + amount;
             cout << amount << " rupees deposited successfully!" << endl;
@@ -37,8 +37,12 @@ public:
 };
 
 int main() {
+
+    cout << "Welcome to the Bank Account Management System!" << endl;
+    cout << "This is a simple demonstration of encapsulation in C++." << endl;
+
     // Creating an object of BankAccount using the constructor
-    BankAccount account1("Rahul", 1000);
+    BankAccount account1("user", 1000);
 
     // This will cause a compilation error because 'balance' is private:
     // account1.balance = 50000; 
