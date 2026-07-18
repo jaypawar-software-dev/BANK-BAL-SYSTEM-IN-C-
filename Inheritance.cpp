e69d58977e99a6879dfdfceb9e3cc135c1f63c83
JAY PAWAR <245702788+jaypawar-software-dev@users.noreply.github.com>
2026-07-18 20:35:55 +0530

    Add files via upload

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -4,27 +4,28 @@ using namespace std;
 // Parent Class (Base Class)
 class Vehicle {
 public:
-    void horn() {
-        cout << "Peep-Peep! 🎺" << endl;
+      void horn() {
+        cout << "Peep-Peep!" << endl;
     }
     void start() {
-        cout << "Vehicle is starting..." << endl;
+            cout << "Vehicle is starting" << endl;
     }
 };
 
 // Child Class (Derived Class) inherits from Vehicle
-class SportsCar : public Vehicle {
+  class SportsCar : public Vehicle {
 public:
     // Function Overriding: Modifying the Parent's start() behavior
     void start() {
-        cout << "Sports Car starts with a loud ROAR! 🏎️" << endl;
+        cout << "Sports Car starts with a loud ROAR! " << endl;
     }
 };
 
-int main() {
-    SportsCar myCar;
-    myCar.horn();  // Calls inherited function from Parent class
-    myCar.start(); // Calls overridden function from Child class
+       int main() {
+       cout << "This is a simple example of Inheritance in C++" << endl;
+     SportsCar myCar;
+        myCar.horn();  // Calls inherited function from Parent class
+      myCar.start(); // Calls overridden function from Child class
 
     return 0;
 }
